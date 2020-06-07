8150a842c7bf0feaf6247418bacf7527c4186277
Shruti <semwalshruti450@gmail.com>
2020-06-07 14:50:46 +0530

    Updates

---
+++ precache-manifest.6bb121e189f8b30c212044cea7b716ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae0426d8b051181d2387ad2c7dbdb3ec",
+    "url": "/findmyevent/index.html"
+  },
+  {
+    "revision": "a9eb23c90879e7875875",
+    "url": "/findmyevent/static/css/2.aee84c9b.chunk.css"
+  },
+  {
+    "revision": "9a1cf95c6a3ca5be62af",
+    "url": "/findmyevent/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "a9eb23c90879e7875875",
+    "url": "/findmyevent/static/js/2.7a00e270.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/findmyevent/static/js/2.7a00e270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a1cf95c6a3ca5be62af",
+    "url": "/findmyevent/static/js/main.36aff6bd.chunk.js"
+  },
+  {
+    "revision": "ae4bb62a35020419d9af",
+    "url": "/findmyevent/static/js/runtime-main.4b2f5f5e.js"
+  }
+]);
